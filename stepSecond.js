d7c3c882915fcbaa794099e87ebde241a9730547
Mitya Kim <mitya_kim@icloud.com>
2021-12-23 11:28:39 -0800

    commit

--- stepSecond.js
+++ stepSecond.js
@@ -10,13 +10,38 @@
 // // -> False
 // addToZero([1, 2, 3, -2]);
 // // -> True
+function addToZero (arr) {
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr.length; j++) {
+            if ((arr[i] + arr[j] === 0) && (arr.lngth > 2) && (i !== j)) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }     
+}
+addToZero([1,2,3,4, -4])
 
 
 // 2) Unique Characters
 // Write a function that takes in a single word, as a string. It 
 // should return True if that word contains only unique characters. 
 // Return False otherwise.
-
+function uniqueChar(word) {
+    var result = false
+    for(let i = 0; i < word.length; i++) {
+        for(let j = 0; j < word.length; j++) {
+            if (word[i] === word[j] && i !== j) {
+                result = false
+            } else {
+                result = true
+            }
+        }
+    }
+    return result
+    console.log(result)
+}
 
 
 
@@ -32,9 +57,26 @@
 
 
 
+
+
 // 4) Longest Word
 // Write a function, find_longest_word, that takes a 
 // list of words and returns the length of the longest one.
 // For example:
 // findLongestWord(["hi", "hello"]);
 // // -> 5
+function findLongestWord(arr) {
+    let max = 0
+    for (let i = 0; i < arr.length; i++){
+      for (let j = 0; j < arr.length; j++) {
+         if (arr[i].length < arr[j].length && i !== j) {
+           console.log(arr[i].length, arr[j].length)
+           max = arr[j]
+           console.log(max)
+         }
+      }
+    }
+    console.log(max.length)
+ }
+ 
+ findLongestWord(['hi', 'hello']);
